a38d8e939bdb8e28e6b6bd26fb8c263844b8205c
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-03-30 12:19:29 -0500

    Array Iteration!

---
+++ MyHTML FIles/JSFiles/ArraysDay2.js
@@ -0,0 +1,7 @@
+var FavNumber = [2,3,56,17,56,17];
+
+    console.log(FavNumber.indexOf(17));
+    console.log(FavNumber.lastIndexOf(17));
+
+    //The slice method similar to a substring method can return twoa value from one or two arguments
+
