8da291a2efea802ce99c118c12fb2dec8adf93cb
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-04-07 09:46:16 -0500

    finsihing up the first problem

--- My_HTML_Files/JSFiles/ForLoopsExercises.js
+++ My_HTML_Files/JSFiles/ForLoopsExercises.js
@@ -1,5 +1,16 @@
 "use strict";
 
+//#2
+    function showMultiplicationTable(x){
+        for(let i = 1; i < 13; i++){
+            var theResult = (x * i);
+            console.log(theResult);
+        }
+
+    }
+console.log(showMultiplicationTable(6));
+
+
 //#3
 for(var i = 0; i < 10; i++){
     var random = Math.floor(Math.random()*181) + 20;
