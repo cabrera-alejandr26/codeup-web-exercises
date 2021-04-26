9b7b38cda46505a8a855dc9dc0c9264ead14a918
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-04-25 20:30:51 -0500

    Some practice

--- My_HTML_Files/JSFiles/Arays-Extra-Exercises.js
+++ My_HTML_Files/JSFiles/Arays-Extra-Exercises.js
@@ -25,18 +25,20 @@ function removeAll(x,y){
     }
     return x;
 }
-
-console.log(removeAll(bugs,"mosquito"));
+console.log('The array bugs without any changes is: ' + bugs);
+console.log(removeAll(bugs,"roach"));
 
 //Exercise #3
+//Make a function called randomIntBetween(min, max) that returns a random number between the min and the max.
 
 function randomIntBetween(min,max){
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(randomIntBetween(4,8));
+console.log(randomIntBetween(4,10));
 
-//Exercise #6
+//Exercise #4
+//Make a function called coinFlip() that returns either 0 or 1, randomly.
 function coinFlip(){
     var theCoin = Math.floor(Math.random() + Math.random() - 1 );
     if(theCoin === -1){
@@ -49,3 +51,33 @@ function coinFlip(){
 }
 
 console.log(coinFlip());
+
+//Exercise #5
+//Make a function called twoDice() that returns the sum of rolling two six sided dice.
+//I have decided that the two dices will be based of six sided dice.
+
+function bothDice() {
+    console.log('The value of dice1 is: ' + diceUno);
+    console.log('The value of dice1 is: ' + diceDos);
+
+    console.log('The total value of both dice is: ' + (diceDos + diceUno));
+
+}
+
+function dice1(max,min) {
+    var dice1Value = Math.floor(Math.random() * (max -min) + 1);
+    var theValue1 = dice1Value;
+    console.log(theValue1);
+    return theValue1;
+}
+function dice2(max,min) {
+    var dice2Value = Math.floor(Math.random() * (max -min) + 1);
+    var theValue2 = dice2Value;
+    console.log(theValue2);
+    return theValue2;
+}
+
+var diceUno = dice1(6,0);
+var diceDos = dice2(6,0);
+
+bothDice();
