95ebda361cb9bde4c4ba003a0e160efc85262552
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-05-09 14:38:00 -0500

    added git ignore

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -1,5 +1,29 @@
 "use strict";
 
-$.ajax("json/blog.json").done(function(blog){
-        console.log(blog);
-})
+
+function Bloggin(){
+        $.ajax("json/blog.json").done(function(blog){
+                console.log(blog);
+                        var theWords = "<div id='theBLog'>";
+                        theWords += "<h1>" + blog[0].title + "</h1>";
+                        theWords += "<p><small>" + blog[0].date + "<br>" + "<strong>" + "Categories: " + "</strong>" + "<a href='#'>" + blog[0].categories + "</a>" + "</small></p>";
+                        theWords += "<p>" + blog[0].content + "</p>";
+                        theWords += "</div>";
+                        $('#posts').append(theWords);
+        }
+)}
+
+function Bloggin2(){
+        $.ajax("json/blog.json").done(function(blog){
+                console.log(blog);
+                        var theWords = "<div id='theBLog2'>";
+                        theWords += "<h1>" + blog[1].title + "</h1>";
+                        theWords += "<p><small>" + blog[1].date + "<br>" + "<strong>" + "Categories: " + "</strong>" + "<a href='#'>" + blog[1].categories + "</a>"  + "</small></p>";
+                        theWords += "<p>" + blog[1].content + "</p>";
+                        theWords += "</div>";
+                        $('#posts').append(theWords);
+        }
+)}
+
+Bloggin();
+Bloggin2();
---
+++ js/keys.js
@@ -0,0 +1,1 @@
+const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiY2FicmVyYS1hbGVqYW5kcjI2IiwiYSI6ImNrb2hrcjB0czBlc3cydnE0eWt4enhhYzkifQ.lCj-RtbNq9kbdJklgt27fQ";
