12adce2b726b19769a48fb477ecca79893fc9ed6
Alejandro Cabrera <cabrera.alejandr26@gmail.com>
2021-05-24 17:26:14 -0500

    morning exercise

--- js/RESTful-API-request.js
+++ js/RESTful-API-request.js
@@ -1,23 +1,50 @@
 //GET METHOD //
 
-let getOptions = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-};
+// let getOptions = {
+//     method: 'GET',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     }
+// };
 
-const getBooks = () => {
-    fetch("https://wool-jolly-numeric.glitch.me/books")
-        .then(resp => resp.json())
-        .then(books => {
-            console.log(books);
-            let htmlStr = "";
-            for (let book of books) {
-                htmlStr += `<h3>${book.title}</h3><p>by: ${book.author.firstName} ${book.author.lastName}</p>`;
-            }
-            $("#container").html(htmlStr);
-        });
+// const getBooks = () => {
+//     fetch("https://wool-jolly-numeric.glitch.me/books")
+//         .then(resp => resp.json())
+//         .then(books => {
+//             console.log(books);
+//             let htmlStr = "";
+//             for (let book of books) {
+//                 htmlStr += `<h3>${book.title}</h3><p>by: ${book.author.firstName} ${book.author.lastName}</p>`;
+//             }
+//             $("#container").html(htmlStr);
+//         });
+// }
+//
+// getBooks();
+
+
+function sayHello(name) {
+    return 'Hello, ' + name + '!';
 }
 
-getBooks();
+console.log(sayHello('alex'));
+
+const sayHello1 = (name) => { 'Hello, ' + name + '!' };
+console.log(sayHello1('alex'));
+const sayHello2 = name => { 'Hello, ' + name + '!' };
+console.log(sayHello2('alex'));
+const sayHello3 = (name) => `Hello, ${name}!`;
+console.log(sayHello3('alex'));
+
+// let numbers = [1, 2, 3, 4, 5];
+// numbers = numbers.map(n => n * 3);
+//
+// console.log(numbers);
+
+const isDivisibleBy3 = n => n % 3 === 0;
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers.filter(isDivisibleBy3));
+console.log(numbers);
+
